4e7343bcc57a64ed8353f21e01e2b7cc2b339e1b
Radoslaw Landowski <radoslaw.landowski@gmail.com>
2023-11-27 21:17:48 +0100

    setup

---
+++ index.js
@@ -0,0 +1,29 @@
+const { SerialPort } = require('serialport')
+const Readline = require('@serialport/parser-readline');
+const { ReadlineParser } = require('@serialport/parser-readline')
+
+
+SerialPort.list().then((value) => console.log(value))
+
+const port = new SerialPort({ path: '/dev/ttyACM1', baudRate: 9600 })
+
+
+port.write('Hello!')
+
+// Read data that is available but keep the stream from entering //"flowing mode"
+port.on('readable', function () {
+    port.write('Hello!')
+
+    port.read();
+});
+
+port.on('data', function (data) {
+
+});
+
+const parser = port.pipe(new ReadlineParser())
+
+
+// const lineStream = port.pipe(new Readline())
+
+parser.on('data', (value) => console.log(value))
