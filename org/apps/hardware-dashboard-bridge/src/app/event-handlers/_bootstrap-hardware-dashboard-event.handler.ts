45ce0dc440f79f6c464ccd5692160331e7f57db6
Radoslaw landowski <radoslaw.l@practi.net>
2023-12-03 11:33:12 +0100

    fix handshake

--- org/apps/hardware-dashboard-bridge/src/app/event-handlers/_bootstrap-hardware-dashboard-event.handler.ts
+++ org/apps/hardware-dashboard-bridge/src/app/event-handlers/_bootstrap-hardware-dashboard-event.handler.ts
@@ -7,17 +7,18 @@ import {WebsocketGateway} from "../outputs/websocket-gateway";
 import {
   _BootstrapHardwareDashboardReceivedEvent
 } from "../contract/events/_bootstrap-hardware-dashboard-received-event";
+import {ArduinoSerialPortConnectionService} from "../hardware/arduino.serial.port.connection.service";
 
 @Injectable()
 export class _BootstrapHardwareDashboardEventHandler {
 
-  constructor(readonly websocketGateway: WebsocketGateway) {
+  constructor(readonly arduinoSerialPortConnectionService: ArduinoSerialPortConnectionService){
   }
 
   @OnEvent(_BootstrapHardwareDashboardReceivedEvent.Queue, { async: true })
     async handle(event: _BootstrapHardwareDashboardReceivedEvent) {
         console.log(`Received event: ${JSON.stringify(event)}`)
 
-        this.websocketGateway.sendMessage(event)
+        await this.arduinoSerialPortConnectionService.write({})
     }
 }
